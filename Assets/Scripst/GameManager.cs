4248ab5304cb056d4915c8a6f82115cc13bc318f
minhdz2005 <minhnqph49938@gmail.com>
2025-04-02 18:12:29 +0700

    Update GameManager.cs

--- Assets/Scripst/GameManager.cs
+++ Assets/Scripst/GameManager.cs
@@ -1,23 +1,37 @@
 ﻿using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+
     private int score = 0;
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private GameObject gameOverUi;
     [SerializeField] private GameObject gameWinUI;
     private bool isGameOver = false;
     private bool isGameWin = false;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);  // Giữ lại GameManager khi chuyển scene
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void Start()
     {
         UpdateScore();
-        gameOverUi.SetActive(false);
-        gameWinUI.SetActive(false);
-
+        if (gameOverUi) gameOverUi.SetActive(false);
+        if (gameWinUI) gameWinUI.SetActive(false);
     }
-
 
     public void AddScore(int points)
     {
@@ -26,27 +40,29 @@ public class GameManager : MonoBehaviour
             score += points;
             UpdateScore();
         }
-
     }
+
     private void UpdateScore()
     {
-        scoreText.text = score.ToString();
+        if (scoreText) scoreText.text = score.ToString();
     }
 
     public void GameOver()
     {
         isGameOver = true;
-        score = 0;
+        score = 0;  // Reset điểm khi thua
         Time.timeScale = 0;
-        gameOverUi.SetActive(true);
+        if (gameOverUi) gameOverUi.SetActive(true);
     }
+
     public void GameWin()
     {
         isGameWin = true;
         Time.timeScale = 0;
-        gameWinUI.SetActive(true);
-        SaveHighScore();  // Lưu điểm cao khi thắng
+        if (gameWinUI) gameWinUI.SetActive(true);
+        SaveHighScore();
     }
+
     private void SaveHighScore()
     {
         int highScore = PlayerPrefs.GetInt("HighScore", 0);
@@ -54,32 +70,40 @@ public class GameManager : MonoBehaviour
         if (score > highScore)
         {
             PlayerPrefs.SetInt("HighScore", score);
-            PlayerPrefs.Save(); // Lưu dữ liệu lại
+            PlayerPrefs.Save();
         }
     }
+
     public void RestartGame()
     {
         isGameOver = false;
         score = 0;
         UpdateScore();
         Time.timeScale = 1;
-
-        // Chỉ load lại màn 1 (Game) sau khi chơi lại
-        SceneManager.LoadScene("Game"); // Đảm bảo rằng tên Scene "Game" đúng với tên màn chơi đầu tiên
+        SceneManager.LoadScene("Game");
     }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
     }
+
     public void GotoMenu()
     {
         SceneManager.LoadScene("Menu");
         Time.timeScale = 1;
     }
+
     public bool IsGameOver()
     {
         return isGameOver;
     }
+
     public bool IsGameWin()
     {
         return isGameWin;
